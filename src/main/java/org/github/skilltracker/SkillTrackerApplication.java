5c95239054cf7cf2e3f200a4913361506600439e
frikit <osipovvictor1994@gmail.com>
2020-03-15 17:13:39 +0000

    add more h2 configs for web app for faster check in db idea auto reformat

--- src/main/java/org/github/skilltracker/SkillTrackerApplication.java
+++ src/main/java/org/github/skilltracker/SkillTrackerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkillTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkillTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SkillTrackerApplication.class, args);
+    }
 
 }
