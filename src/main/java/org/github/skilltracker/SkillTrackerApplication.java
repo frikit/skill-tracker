b1e7d88a85dfef07604886db53aa0d4ccc341060
frikit <osipovvictor1994@gmail.com>
2020-03-15 17:01:20 +0000

    Generate from start spring io

---
+++ src/main/java/org/github/skilltracker/SkillTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.github.skilltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/github/skilltracker/SkillTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.skilltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
