fdb4fb62f42dd73c4ac45dabdc87d43856fb3e67
frikit <osipovvictor1994@gmail.com>
2020-03-15 19:09:24 +0000

    improve little bit test for spring context(test from spring io docs)

--- src/test/java/org/github/skilltracker/SkillTrackerApplicationTests.java
+++ src/test/java/org/github/skilltracker/SkillTrackerApplicationTests.java
@@ -2,13 +2,19 @@ package org.github.skilltracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SkillTrackerApplicationTests {
 
+    @LocalServerPort
+    int randomServerPort;
+
     @Test
-    void testContextIsLoaded() {
-        assert true;
+    public void testContextIsUpAndRunning() {
+        assertTrue(randomServerPort > 1);
     }
 
 }
