e5a119bcde22e347ea64108287d1f180597d1ee4
frikit <osipovvictor1994@gmail.com>
2020-03-15 18:03:21 +0000

    add simpliest version for employee controller to be used to test how rest is working fine and in db is inserting fine

---
+++ src/main/java/org/github/skilltracker/controller/EmployeeController.java
@@ -0,0 +1,47 @@
+package org.github.skilltracker.controller;
+
+import org.github.skilltracker.model.Employee;
+import org.github.skilltracker.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    private EmployeeRepository repository;
+
+    @Autowired
+    public EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping(value = "/employees", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Employee> getAllEmployees() {
+        return (List<Employee>) repository.findAll();
+    }
+
+    @PutMapping(value = "/employee/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Employee putEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setFirstName(newEmployee.getFirstName());
+                    employee.setLastName(newEmployee.getLastName());
+                    employee.setUsername(newEmployee.getUsername());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> repository.save(newEmployee));
+    }
+
+    @PostMapping(value = "/employee", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Employee postEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
