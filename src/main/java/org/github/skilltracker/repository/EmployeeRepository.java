79783a90c16a35eae87ac25bee355c7d8a69a828
frikit <osipovvictor1994@gmail.com>
2020-03-15 19:10:00 +0000

    forget to commit employee repo with new added methods for employee controller

--- src/main/java/org/github/skilltracker/repository/EmployeeRepository.java
+++ src/main/java/org/github/skilltracker/repository/EmployeeRepository.java
@@ -3,5 +3,13 @@ package org.github.skilltracker.repository;
 import org.github.skilltracker.model.Employee;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+    Optional<Employee> findByUsername(String username);
+
+    List<Employee> findByFirstName(String name);
+
 }
